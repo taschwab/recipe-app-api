f761be570ab11bd5667a5b4ae2eb8dd4ca1ae62c
Tyler Schwab <tyleraschwab@gmail.com>
2022-12-02 09:26:31 -0800

    Added GitHub Actions Three.

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,9 @@
 """
 Sample Tests
 """
-from django.test import SimpleTestCase 
+from django.test import SimpleTestCase
 
-from app import calc 
+from app import calc
 
 
 class CalcTests(SimpleTestCase):
