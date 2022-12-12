5785bad3e5bab60b86395f16a831b40a72a4f793
Tyler Schwab <tyleraschwab@gmail.com>
2022-12-12 12:25:08 -0800

    Added GitHub Actions.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,7 +24,7 @@ class AdminSiteTests(TestCase):
             name='Test User'
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
@@ -39,5 +39,9 @@ class AdminSiteTests(TestCase):
 
         self.assertEqual(res.status_code, 200)
 
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
 
-
+        self.assertEqual(res.status_code, 200)
