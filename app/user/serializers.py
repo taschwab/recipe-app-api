73fa208b279ad2eebfa058cc753fa0909309b3c0
Tyler Schwab <tyleraschwab@gmail.com>
2022-12-21 12:18:40 -0800

    Added GitHub Actions.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -53,7 +53,7 @@ class AuthTokenSerializer(serializers.Serializer):
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials.')
-            raise serializers.ValidationError(msg, code='author')
+            raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
         return attrs
