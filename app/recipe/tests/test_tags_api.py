c881155a47dc92414c3a2c1a60b7ebef7299432f
Tyler Schwab <tyleraschwab@gmail.com>
2022-12-21 12:49:55 -0800

    Added GitHub Actions.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -72,14 +72,14 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
 
-        def test_update_tag(self):
-            """Test updating a tag."""
-            tag = Tag.objects.create(user=self.user, name='After Dinner')
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
 
-            payload = {'name': 'Dessert'}
-            url = detail_url(tag.id)
-            res = self.client.patch(url, payload)
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
 
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            tag.refresh_from_db()
-            self.assertEqual(tag.name, payload['name'])
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -39,9 +39,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(mixins.UpdateModelMixin,
-                mixins.ListModelMixin,
-                viewsets.GenericViewSet):
-    """Manage Tags in the database."""
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+    """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
